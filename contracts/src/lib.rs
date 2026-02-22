8887df0efcd5cb86ed2f1975f381ac57fd6dbe95
Anonfedora <musaeleazar090@gmail.com>
2026-02-22 13:22:26 +0100

    feat: Payment Escrow

--- contracts/src/lib.rs
+++ contracts/src/lib.rs
@@ -1,8 +1,19 @@
 #![no_std]
 
+#[path = "booking/lib.rs"]
 pub mod booking;
+
+#[path = "airline/lib.rs"]
 pub mod airline;
+
+#[path = "refund/lib.rs"]
 pub mod refund;
+
+#[path = "governance/lib.rs"]
 pub mod governance;
+
+#[path = "loyalty/lib.rs"]
 pub mod loyalty;
+
+#[path = "token/lib.rs"]
 pub mod token;
